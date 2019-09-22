366b23fdf59806cdf85bc30a3235d22738c57bd8
Lincoln Anderson <landerson@threefivetwo.com>
2019-09-22 16:42:45 -0400

    add styles

---
+++ src/styles.js
@@ -0,0 +1,74 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  animationCanvas: {
+    backgroundColor: 'black',
+    height: 160,
+  },
+  centerContent: {
+    justifyContent: 'center',
+  },
+  centerItems: {
+    alignItems: 'center',
+  },
+  centerContentAndItems: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mb1: {
+    marginBottom: 4,
+  },
+  mb2: {
+    marginBottom: 8,
+  },
+  mb3: {
+    marginBottom: 12,
+  },
+  mb4: {
+    marginBottom: 16,
+  },
+  shapeA: {
+    width: 80,
+    height: 80,
+    backgroundColor: 'aquamarine',
+    borderRadius: 4,
+  },
+  p2: {
+    padding: 8,
+  },
+  p4: {
+    padding: 16,
+  },
+  py1: {
+    paddingVertical: 4,
+  },
+  py2: {
+    paddingVertical: 8,
+  },
+  px4: {
+    paddingHorizontal: 16,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  spaceBetweenContent: {
+    justifyContent: 'space-between',
+  },
+  bgAlmostWhite: {
+    backgroundColor: '#eee',
+  },
+  bgBlack: {
+    backgroundColor: '#000',
+  },
+  fontWeightBold: {
+    fontWeight: 'bold',
+  },
+  uppercase: {
+    textTransform: 'uppercase',
+  },
+  muted: {
+    color: '#888',
+  },
+});
+
+export default styles;
