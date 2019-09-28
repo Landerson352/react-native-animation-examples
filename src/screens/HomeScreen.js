214a4acfce4e6874fe5ee7afb3e6d7c03d86275e
Lincoln Anderson <landerson@threefivetwo.com>
2019-09-28 15:41:46 -0400

    clean up HomeScreen, add intro text

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { FlatList, View, TouchableOpacity } from 'react-native';
 
 import ROUTE from '../route';
+import s from '../styles';
+import Text from '../components/Text';
 import useNavigation from '../utils/useNavigation';
 
 const menuRoutes = [
@@ -23,8 +25,8 @@ const Item = ({ route }) => {
 
   return (
     <TouchableOpacity onPress={() => navigate(key)}>
-      <View style={styles.item}>
-        <Text style={styles.itemText}>{title}</Text>
+      <View style={[s.mb2, s.p4, s.bgWhite, s.rounded]}>
+        <Text b>{title}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -34,33 +36,25 @@ const renderItem = ({ item }) => (
   <Item route={item} />
 );
 
+const header = (
+  <>
+    <Text h1 p>Welcome!</Text>
+    <Text p>
+      Here we demonstrate the key capabilities of various React Native animation libraries,
+      and give you clues and resources to dig deeper on the ones that are most applicable to your app.
+    </Text>
+  </>
+);
+
 const HomeScreen = () => (
-  <View style={styles.homeScreen}>
+  <View style={[s.bgAlmostWhite, s.flexGrow]}>
     <FlatList
-      contentContainerStyle={styles.flatListContentContainer}
+      ListHeaderComponent={header}
+      contentContainerStyle={s.pcontent}
       data={menuRoutes}
       renderItem={renderItem}
     />
   </View>
 );
-
-const styles = StyleSheet.create({
-  homeScreen: {
-    backgroundColor: '#f3f3f3',
-    flex: 1,
-  },
-  flatListContentContainer: {
-    padding: 16,
-  },
-  item: {
-    marginBottom: 8,
-    padding: 16,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-  },
-  itemText: {
-    fontWeight: 'bold',
-  },
-});
 
 export default HomeScreen;
