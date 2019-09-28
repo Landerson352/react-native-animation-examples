4a980481c5e57eb6fd70275596c5f9f5695fc7dc
Lincoln Anderson <landerson@threefivetwo.com>
2019-09-28 11:50:01 -0400

    add padded scrolling to placeholder pages

--- src/screens/MotionScreen.js
+++ src/screens/MotionScreen.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { ScrollView, View } from 'react-native';
 
 import RouteInfo from '../components/RouteInfo';
+import s from '../styles';
 
 export default () => (
-  <RouteInfo />
+  <ScrollView>
+    <View style={s.pcontent}>
+      <RouteInfo />
+    </View>
+  </ScrollView>
 );
