97720ef5d0cb9d38273bff1c3f9e60aaa282c385
Lincoln Anderson <landerson@threefivetwo.com>
2019-09-29 23:43:02 -0400

    add lottie demo

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { YellowBox } from 'react-native';
 
 import AppNavigator from './src/AppNavigator';
 
-YellowBox.ignoreWarnings(['useNativeDriver']);
+YellowBox.ignoreWarnings([
+  'LottieAnimationView.getConstants', // Lottie has a recurring warning with Expo that doesn't impact performance.
+  'useNativeDriver', // Pose noisily surfaces warnings about animations that can't use the nativeDriver.
+]);
 
 export default AppNavigator;
--- src/screens/LottieScreen.js
+++ src/screens/LottieScreen.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import LottieView from 'lottie-react-native';
 
+import AnimationCanvas from '../components/AnimationCanvas';
+import Card from '../components/Card';
+import ExamplesContainer from '../components/ExamplesContainer';
 import RouteInfo from '../components/RouteInfo';
+import Text from '../components/Text';
 import s from '../styles';
 
-export default () => (
-  <ScrollView>
-    <RouteInfo style={s.pcontent} />
-  </ScrollView>
-);
+const EXAMPLE_BODYMOVIN_DATA = require('../../assets/example-bodymovin-data.json');
+
+const styles = StyleSheet.create({
+  example1AnimationCanvas: {
+    backgroundColor: 'rgb(0, 218, 98)',
+  },
+});
+
+export default () => {
+  const animation1Ref = useRef(null);
+
+  useEffect(() => {
+    animation1Ref.current.play();
+  }, []);
+
+  return (
+    <ScrollView>
+      <RouteInfo style={s.pcontent} />
+
+      <ExamplesContainer>
+
+        <Card>
+          <View style={s.pcontent}>
+            <Text h3 style={s.mb2}>Basic animation</Text>
+            <Text>
+              Once the JSON data is available, a Lottie element can be added to a layout quite easily.
+              It can be resized, or allowed to fit the parent element.
+            </Text>
+          </View>
+          <AnimationCanvas style={styles.example1AnimationCanvas}>
+            <LottieView
+              ref={animation1Ref}
+              source={EXAMPLE_BODYMOVIN_DATA}
+            />
+          </AnimationCanvas>
+        </Card>
+
+      </ExamplesContainer>
+    </ScrollView>
+  );
+}
