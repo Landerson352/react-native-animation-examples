ef2cecfbb7939af78541091a6bb4bcc21d06f2d4
Lincoln Anderson <landerson@threefivetwo.com>
2019-10-01 21:18:22 -0400

    add react-native-animatable-unmountable demo

--- src/screens/AnimatableScreen.js
+++ src/screens/AnimatableScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ScrollView, Switch, View } from 'react-native';
-import * as Animatable from 'react-native-animatable';
+import * as Animatable from 'react-native-animatable-unmountable';
 
 import AnimationCanvas from '../components/AnimationCanvas';
 import Card from '../components/Card';
@@ -10,9 +10,11 @@ import RouteInfo from '../components/RouteInfo';
 import Text from '../components/Text';
 import s from '../styles';
 
-const AnimatableView = Animatable.createAnimatableComponent(View);
+const AnimatableView = Animatable.animatableUnmountable(View);
 
 const AnimatableScreen = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
     <ScrollView>
       <RouteInfo style={s.pcontent} />
@@ -21,7 +23,7 @@ const AnimatableScreen = () => {
 
         <Card>
           <View style={s.pcontent}>
-            <Text h3 style={s.mb2}>Basic animation</Text>
+            <Text h3 style={s.mb2}>Basic looping animation</Text>
             <Text>
               Components can be made "Animatable", and come with numerous preset
               animations that can be adjusted for duration and easing, and looped.
@@ -34,6 +36,31 @@ const AnimatableScreen = () => {
               style={s.shapeA}
             />
           </AnimationCanvas>
+        </Card>
+
+        <Card>
+          <View style={s.pcontent}>
+            <Text h3 style={s.mb2}>Mount/unmount transition</Text>
+            <Text>
+              With the unofficial "react-native-animatable-unmountable" library,
+              you can mount and unmount the component with unique animations.
+              Note that interrupted transitions may not hand off smoothly.
+            </Text>
+          </View>
+          <AnimationCanvas>
+            <AnimatableView
+              mounted={isVisible}
+              animation="flipInX"
+              duration={300}
+              unmountAnimation="flipOutX"
+              unmountDuration={300}
+              style={s.shapeA}
+            />
+          </AnimationCanvas>
+          <ControlRow>
+            <Text>Toggle visibility</Text>
+            <Switch onValueChange={setIsVisible} value={isVisible} />
+          </ControlRow>
         </Card>
 
       </ExamplesContainer>
