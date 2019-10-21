b294cde50d254a6dfbd1a4a76c50d5b8a93a627e
Lincoln Anderson <landerson@threefivetwo.com>
2019-10-20 22:29:47 -0400

    add redash demo

--- src/screens/ReanimatedScreen.js
+++ src/screens/ReanimatedScreen.js
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react';
 import { View } from 'react-native';
 import Reanimated from 'react-native-reanimated';
+import * as Redash from 'react-native-redash';
+import { Svg, Path } from 'react-native-svg';
 
 import AnimationCanvas from '../components/AnimationCanvas';
 import Card from '../components/Card';
@@ -9,23 +11,35 @@ import RouteInfo from '../components/RouteInfo';
 import s from '../styles';
 import Text from '../components/Text';
 
+const AnimatedPath = Reanimated.createAnimatedComponent(Path);
+
 const ReanimatedScreen = () => {
   // Note how these variables can be "locked in" with useRef (this is not required).
   // In other words, these animations are not driven by JS re-renders.
   // Instead, you create the connections between logical "nodes", then animations run natively.
 
-  // Example 1: Scroll tracking
   const scrollPosition = useRef(new Reanimated.Value(0)).current;
+
+  // Example 1: Scroll tracking
   const handleScroll = useRef(Reanimated.event([{
     nativeEvent: { contentOffset: { y: scrollPosition } },
   }])).current;
-  const left = useRef(Reanimated.sub(scrollPosition, 280)).current;
+  const left = useRef(Reanimated.sub(scrollPosition, 340)).current;
   const example1ShapeStyle = useRef({ left }).current;
 
-  // Example 2: Clock-based animation
+  // Example 2: SVG path morphing
+  const pathData = useRef(Redash.interpolatePath(scrollPosition, {
+    inputRange: [500, 1100],
+    outputRange: [
+      'M50,100 C77.6142375,100 100,77.6142375 100,50 C100,22.3857625 77.6142375,0 50,0 C22.3857625,0 0,22.3857625 0,50 C0,77.6142375 22.3857625,100 50,100 Z',
+      'M50,57.8040432 C157.751351,57.8040432 57.8040432,157.751351 57.8040432,50 C57.8040432,-57.7513506 157.751351,42.1959568 50,42.1959568 C-57.7513506,42.1959568 42.1959568,-57.7513506 42.1959568,50 C42.1959568,157.751351 -57.7513506,57.8040432 50,57.8040432 Z',
+    ],
+  })).current;
+
+  // Example 3: Clock-based animation
   const clock = useRef(new Reanimated.Clock()).current;
   const rotate = useRef(Reanimated.concat(Reanimated.divide(clock, 4), 'deg')).current;
-  const example2ShapeStyle = useRef({ transform: [{ rotate }] }).current;
+  const example3ShapeStyle = useRef({ transform: [{ rotate }] }).current;
   Reanimated.useCode(Reanimated.block([
     Reanimated.startClock(clock),
   ]), [clock]);
@@ -58,6 +72,20 @@ const ReanimatedScreen = () => {
 
         <Card>
           <View style={s.pcontent}>
+            <Text h3 style={s.mb2}>SVG path morphing</Text>
+            <Text>
+              With the Redash library, you can interpolate SVG path data, and more.
+            </Text>
+          </View>
+          <AnimationCanvas>
+            <Svg width={100} height={100}>
+              <AnimatedPath d={pathData} fill="aquamarine" />
+            </Svg>
+          </AnimationCanvas>
+        </Card>
+
+        <Card>
+          <View style={s.pcontent}>
             <Text h3 style={s.mb2}>Clock-based animation</Text>
             <Text>
               By starting a "clock" and driving a rotation transform off of it,
@@ -65,7 +93,7 @@ const ReanimatedScreen = () => {
             </Text>
           </View>
           <AnimationCanvas>
-            <Reanimated.View style={[s.shapeA, example2ShapeStyle]} />
+            <Reanimated.View style={[s.shapeA, example3ShapeStyle]} />
           </AnimationCanvas>
         </Card>
 
