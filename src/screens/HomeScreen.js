c490cfd455071df93b31e143091b649e373ac385
Lincoln Anderson <landerson@threefivetwo.com>
2020-01-18 19:44:14 -0500

    add header to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -28,9 +28,20 @@ const renderItem = ({ item }) => (
   <Item route={item} />
 );
 
+const header = (
+  <Text p>
+    These examples are broken up into two sections.
+    "{ROUTE.APIS_AND_LIBRARIES.title}" demonstrates various
+    React Native APIs as well as libraries available on NPM.
+    "{ROUTE.DESIGN_PATTERNS.title}" shows you some potential approaches
+    you can use to achieve a specific animated effect in React Native.
+  </Text>
+);
+
 const HomeScreen = () => (
   <View style={[s.bgAlmostWhite, s.grow1]}>
     <FlatList
+      ListHeaderComponent={header}
       contentContainerStyle={s.pcontent}
       data={menuRoutes}
       renderItem={renderItem}
