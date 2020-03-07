7f511d2c0ce6f1a82a3197a127bfaf337f2f1f03
Lincoln Anderson <landerson@threefivetwo.com>
2020-03-07 13:22:19 -0500

    ignore more warnings from libs

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { YellowBox } from 'react-native';
 import AppNavigator from './src/AppNavigator';
 
 YellowBox.ignoreWarnings([
+  'componentWillMount', // Animated Sprite needs to be updated per this deprecation warning
+  'componentWillReceiveProps', // Animatable needs to be updated per this deprecation warning
   'LottieAnimationView.getConstants', // Lottie has a recurring warning with Expo that doesn't impact performance.
   'useNativeDriver', // Pose noisily surfaces warnings about animations that can't use the nativeDriver.
 ]);
